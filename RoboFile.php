5665506354b303666688dd16a158da80bfe9cca1
Skyler Richter <skylerrichter@gmail.com>
2017-07-16 13:07:40 -0700

    Basic post rendering.

--- RoboFile.php
+++ RoboFile.php
@@ -9,27 +9,67 @@ use Michelf\Markdown;
 class RoboFile extends Tasks
 {
 	/**
-	 * Description.
+	 * @var string
+	 */
+	protected $buildPath = 'build';
+
+	/**
+	 * @var string
+	 */
+	protected $contentPath = 'posts';
+
+	/**
+	 * @var string
+	 */
+	protected $layoutPath = 'templates/layout.mustache';
+
+	/**
+	 * Return a new MustacheEngine instance.
 	 * 
 	 * @return MustacheEngine
 	 */
 	protected function getLayout()
 	{
-		return (new MustacheEngine())->loadTemplate('{{ post }}');
+		return (new MustacheEngine())->loadTemplate($this->loadLayout());
 	}
 
 	/**
-	 * Description.
+	 * Load the layout.
+	 * 
+	 * @return string
+	 */
+	public function loadLayout()
+	{
+		return file_get_contents($this->layoutPath);
+	}
+
+	/**
+	 * Make content path.
 	 * 
 	 * @return string
 	 */
 	protected function makePath($file)
 	{
-		return sprintf('build/posts/%shtml', $file->getBasename($file->getExtension()));
+		return sprintf(
+			'%s/%s/%s.html', 
+			$this->buildPath, 
+			$this->contentPath, 
+			$file->getFilename()
+		);
+	}
+
+	/**
+	 * Run getContents threw Markdown.
+	 * 
+	 * @return string
+	 */
+	public function markdownFileContents($file)
+	{
+		return Markdown::defaultTransform($file->getContents());
 	}
 
     /**
-	 * Description.
+	 * Compile website.
 	 * 
 	 * @return void
      */
@@ -37,25 +77,25 @@ class RoboFile extends Tasks
     {
     	$this->compileAssets();
 
-    	$files = Finder::create()->files()->name('*.md')->in('posts');
+    	$files = Finder::create()->files()->name('*.md')->in($this->contentPath);
 
     	foreach ($files as $file) {
     		$this
     			->taskWriteToFile($this->makePath($file))
-     			->line($this->getLayout()->render(['post' => 'World!']))
+     			->line($this->getLayout()->render(['content' => $this->markdownFileContents($file)]))
 		    	->run();
     	}
     }
 
     /**
-	 * Description.
+	 * Compile assets.
 	 * 
 	 * @return void
      */
     public function compileAssets()
     {
     	$this
-			->taskScss(['scss/index.scss' => 'build/css/index.css'])
+			->taskScss(['scss/index.scss' => sprintf('%s/css/index.css', $this->buildPath)])
             ->importDir('scss/imports')
             ->run();
     }
